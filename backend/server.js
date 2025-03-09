3c148ebbbaaf2c4820eeb1e4ebd0368b2ab0d6e1
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-03-09 14:11:55 -0300

    create payment controller, route and stripe

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
 
 
 
@@ -22,6 +23,9 @@ app.use("/api/auth", authRoutes)
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
+app.use("/api/payments", paymentRoutes);
+
+
 
 app.listen(PORT, ()=> {
     console.log("server is running on http://localhost:"+ PORT);
