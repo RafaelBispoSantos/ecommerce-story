a32276a9df0c1b38ea0e264adc822839007f7479
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-03-09 14:52:57 -0300

    function getProfile

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -156,6 +156,10 @@ export const refreshTokens = async (req, res) => {
     }
 };
 
-// export const getProfile = async (req, res)=> {
-
-// }
+export const getProfile = async (req, res) => {
+	try {
+		res.json(req.user);
+	} catch (error) {
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {  login, logout, refreshTokens, signup } from '../controllers/auth.controller.js';
+import {  getProfile, login, logout, refreshTokens, signup } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ router.post("/signup", signup);
 router.post("/login",login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshTokens);
-// router.get("/profile", getProfile);
+router.get("/profile", protectRoute, getProfile);
+
 
 export default router; 
