13c4f283ca7d4d910830b4a722bfb7bfc51dae00
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-03-16 00:48:26 -0300

    Adiciona configuração do Tailwind CSS e ajusta arquivos de estilo e configuração do Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,13 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite';
 import react from "@vitejs/plugin-react";
-import dotenv from 'dotenv';
 
-// Carregar variáveis de ambiente do arquivo .env
-dotenv.config();
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target:  "http://localhost:5000", // Usa a variável de ambiente ou fallback para localhost
-        changeOrigin: true,
-        secure: false, // Defina `true` se o backend for HTTPS
-      },
-    },
-  },
-});
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},})
