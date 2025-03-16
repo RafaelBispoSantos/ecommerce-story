55541975fb897b57e9ef0dce3178ce996bfaa2cc
Rafael Bispo <rafael_bispo23@hotmail.com>
2025-03-16 14:18:59 -0300

    Atualiza dependências do projeto, adiciona configuração .npmrc e melhora a configuração do Vite com autoprefixer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-
-// Remova temporariamente o import do plugin React
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer'; // Importe o autoprefixer
 
 export default defineConfig({
-  // Use uma verificação condicional para o plugin
-  plugins: [], 
+  plugins: [react()],
   css: {
     postcss: {
-      plugins: [],  // Configuração mínima de PostCSS sem autoprefixer
+      plugins: [
+        autoprefixer(), // Adicione o autoprefixer
+      ],
     },
-  },// Temporariamente vazio para o deploy
+  },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
       },
     },
   },
